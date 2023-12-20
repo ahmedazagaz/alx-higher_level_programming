be7090b2d9da7e776351a921c912bbaecc99a441
ahmedazagaz <contactwithazagaz@gmail.com>
2023-12-19 18:03:39 -0800

    python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A class that represents a square"""
+
+
+class Square:
+    """A class that represents a square"""
+    def __init__(self, size):
+        """constructor
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  #: size of the square
