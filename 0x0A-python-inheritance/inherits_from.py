98d5532446dbf14a67fa6089db81362e301d01b9
ahmedazagaz <contactwithazagaz@gmail.com>
2024-01-08 04:12:08 -0800

    python

---
+++ 0x0A-python-inheritance/inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Method for the inherits_from method """
+
+
+def inherits_from(obj, a_class):
+    """
+    Determines if an object is a true subclass of a class.
+    """
+    return isinstance(obj, a_class) and type (obj) != a_class
