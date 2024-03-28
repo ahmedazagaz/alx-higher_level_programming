fa7a59a662ac06df36776fe0963928a73c220270
ahmedazagaz <contactwithazagaz@gmail.com>
2024-03-28 12:38:39 -0700

    python

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""This script takes a URL sends a POST request to 
-the passed URL takes email as a parameter
-"""
+"""UrlLibRequest"""
+from urllib import parse, request
 import sys
-import urllib.parse
-import urllib.request
 
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+if __name__ == '__main__':
+    data = parse.urlencode({"email": sys.argv[2]})
+    data = data.encode()
+    with request.urlopen(sys.argv[1], data) as res:
+        body = res.read()
+    print(body.decode("utf8"))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""This script that takes in a URL, sends 
-
-- a request to the URL, displays the body 
-- of the response (decoded in utf-8).
-"""
+"""UrlLibRequest"""
+from urllib import error, request
+import sys
 
 
-if __name__ == "__main__":
-    import sys
-    from urllib import request, error
-
+if __name__ == '__main__':
     try:
         with request.urlopen(sys.argv[1]) as res:
-            print(res.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code:', er.code)
+            body = res.read()
+            print(body.decode("utf8"))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
